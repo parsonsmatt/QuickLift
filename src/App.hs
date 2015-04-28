41055c207c2e387fe390c670bb4f911b39bdab12
Matt Parsons <parsonsmatt@gmail.com>
2015-04-27 22:43:14 -0400

    Add a bunch of routes!

--- src/App.hs
+++ src/App.hs
@@ -9,15 +9,61 @@ import Views(indexV)
 
 app :: ScottyM ()
 app = do
-    get     "/"         indexH
-    get     "/:word"    wordH
+    get     "/"             indexH
+    get     "/about"        aboutH
+    get     "/contact"      contactH
+    get     "/login"        loginH
+    get     "/register"     registerH
+    get     "/sessions"     sessionsH
+    post    "/sessions"     postSessionsH
+    get     "/sessions/:id" getSessionH
+    get     "/sessions/:id/edit" editSessionH
+    get     "/:word"        wordH
 
 
 indexH :: ActionM ()
 indexH = indexV
 
 
+aboutH :: ActionM ()
+aboutH = html "This is a weightlifting app!"
+
+
+contactH :: ActionM ()
+contactH = html "Contact me at parsonsmatt@gmail.com!"
+
+
+loginH :: ActionM ()
+loginH = html "Login!"
+
+
+registerH :: ActionM ()
+registerH = html "Register!"
+
+
+sessionsH :: ActionM ()
+sessionsH = html "List of your sessions!"
+
+
+postSessionsH :: ActionM ()
+postSessionsH = do
+    session <- param "session"
+    html $ "You posted a session: " <> session <> "!"
+
+
+getSessionH :: ActionM ()
+getSessionH = do
+    sessionId <- param "id"
+    html $ "Requesting Session " <> sessionId <> "!"
+
+
+editSessionH :: ActionM ()
+editSessionH = do
+    sessionId <- param "id"
+    html $ "Editing session " <> sessionId <> "!"
+    
 wordH :: ActionM ()
 wordH = do
     beam <- param "word"
     html $ "uh you said " <> beam <> "!"
+
