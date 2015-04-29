3c3dd1f961271985bb74a9af1a918951ada1939c
Matt Parsons <parsonsmatt@gmail.com>
2015-04-28 23:14:02 -0400

    Add layout stuff

--- src/Views/Layout.hs
+++ src/Views/Layout.hs
@@ -1,8 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Views.Layout(
-    
+      headL
+    , headerL
+    , footerL
   ) where
 
 import Web.Scotty ()
 
+import Text.Blaze.Html5            as H
+import Text.Blaze.Html5.Attributes
+
+headL :: Html
+headL = docTypeHtml $ 
+    H.head $ 
+        H.title "QuickLift"
+    
+headerL :: Html
+headerL = 
+    header $ do
+        h1 "QuickLift"
+        H.p $ do
+            H.a ! href "register" $ "Register" 
+            H.a ! href "login" $ "Login"
+            H.a ! href "sessions" $ "Sessions"
+
+
+footerL :: Html
+footerL = undefined
