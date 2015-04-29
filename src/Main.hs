948b413de0d31065070030f3140bce89d28a41d9
Matt Parsons <parsonsmatt@gmail.com>
2015-04-29 00:03:00 -0400

    Add middleware for static stuff

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.Static
 
 import Web.Scotty
 
@@ -10,4 +11,5 @@ import App
 main :: IO ()
 main = scotty 3000 $ do
     middleware logStdoutDev
+    middleware $ staticPolicy (noDots >-> addBase "static")
     app
