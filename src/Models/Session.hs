019e5db1e24ce1adf7f4803539c68a6ed43839fd
Matt Parsons <parsonsmatt@gmail.com>
2015-04-30 20:10:34 -0400

    Add Text

--- src/Models/Session.hs
+++ src/Models/Session.hs
@@ -2,5 +2,11 @@
 
 module Models.Session where
 
--- import Data.Dates
+import Data.Dates
+import Data.Text as T
+
+data Session = Session { date :: DateTime
+                       , text :: Text 
+                       }
+
 
