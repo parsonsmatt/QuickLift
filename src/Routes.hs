e8008e237ce37942d55167d35d267c95e2d1b560
Matt Parsons <parsonsmatt@gmail.com>
2015-05-02 18:35:44 -0400

    refactor routes

--- src/Routes.hs
+++ src/Routes.hs
@@ -1,7 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Routes(
-    sessionsRoutes
+      routes
     ) where
 
-import Routes.Sessions
+import Web.Scotty
+import Text.Blaze.Html5 (Html)
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
+import Routes.Sessions(sessionsRoutes)
+
+import Views(homeV)
+
+routes :: ScottyM ()
+routes = do
+    get     "/"             homeH
+    get     "/about"        aboutH
+    get     "/contact"      contactH
+    get     "/login"        loginH
+    get     "/register"     registerH
+    sessionsRoutes
+
+
+blaze :: Html -> ActionM ()
+blaze = html . renderHtml
+
+homeH :: ActionM ()
+homeH = blaze homeV 
+
+
+aboutH :: ActionM ()
+aboutH = html "This is a weightlifting app!"
+
+
+contactH :: ActionM ()
+contactH = html "Contact me at parsonsmatt@gmail.com!"
+
+
+loginH :: ActionM ()
+loginH = html "Login!"
+
+
+registerH :: ActionM ()
+registerH = html "Register!"
