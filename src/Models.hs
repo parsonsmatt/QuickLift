a55e5009afc919cbb4d43a3db3e6e3b6a19349fb
Matt Parsons <parsonsmatt@gmail.com>
2015-05-06 19:29:23 -0400

    Define persistent model

--- src/Models.hs
+++ src/Models.hs
@@ -1,7 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Models where
 
+import Database.Persist
+import Database.Persist.Postgresql
+import Database.Persist.TH
+
+import Data.DateTime
+import Data.Text as T
+
 import Models.Session()
 
-
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Session
+    sessionText Text
+    date DateTime
+    deriving Show
+|]
