ba00ae67d04ebc58ddedaeedfa65426f1a6da1e7
Matt Parsons <parsonsmatt@gmail.com>
2015-05-10 17:16:53 -0400

    switch over to scottyT

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
 module Main where
 
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Middleware.Static
 
-import Web.Scotty
+import Web.Scotty.Trans
 
 import App
 
 main :: IO ()
-main = scotty 3000 $ do
+main = scottyT 3000 id id $ do
     middleware logStdoutDev
     middleware $ staticPolicy (noDots >-> addBase "static")
     app
