d387678f2113c6a7bc13e63eb88b2a5a515904bd
Matt Parsons <parsonsmatt@gmail.com>
2015-11-15 14:35:40 -0500

    TEMPLATE HASKELL

--- src/Users.hs
+++ src/Users.hs
@@ -1,16 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Users 
-  ( WU.LoginId
-  , WU.CreateUserError(..)
-  , WU.UpdateUserError(..)
-  , initUserBackend
-  , destroyUserBackend
-  , housekeepBackend
-  , getUserIdByName
-  , getUserById
-  , functionLevels
-  ) where
+module Users where
 
 import qualified Web.Users.Persistent as WU
 import qualified Web.Users.Types as WU
@@ -32,128 +22,15 @@ backend = do
   pool <- asks getPool
   return $ WU.Persistent (`runSqlPool` pool)
 
-initUserBackend :: AppM ()
-initUserBackend = backend >>= liftIO . WU.initUserBackend
+$(deriveReader 'backend)
 
-destroyUserBackend :: AppM ()
-destroyUserBackend = backend >>= liftIO . WU.destroyUserBackend
-
-housekeepBackend :: AppM ()
-housekeepBackend = backend >>= liftIO . WU.housekeepBackend
+initUserBackend' :: AppM ()
+initUserBackend' = backend >>= \b -> liftIO (WU.initUserBackend b)
 
 getUserIdByName :: Text -> AppM (Maybe WU.LoginId)
-getUserIdByName name = do
-  b <- backend
-  liftIO (WU.getUserIdByName b name)
-
 getUserById :: WU.LoginId -> AppM (Maybe QLUser)
-getUserById id = do
-  b <- backend
-  liftIO (WU.getUserById b id)
-
 listUsers :: Maybe (Int64, Int64) -> AppM [(WU.LoginId, QLUser)]
-listUsers range = do
-  b <- backend
-  liftIO (WU.listUsers b range)
-
 countUsers :: AppM Int64
-countUsers = backend >>= liftIO . WU.countUsers
-
 createUser :: QLUser -> AppM (Either WU.CreateUserError WU.LoginId)
-createUser u = do
-  b <- backend 
-  liftIO (WU.createUser b u)
-
 updateUser :: WU.LoginId -> (QLUser -> QLUser) -> AppM (Either WU.UpdateUserError ())
-updateUser u f = do
-  b <- backend
-  liftIO (WU.updateUser b u f)
-
 updateUserDetails :: WU.LoginId -> (UserDetails -> UserDetails) -> AppM ()
-updateUserDetails u f = do
-  b <- backend
-  liftIO (WU.updateUserDetails b u f)
-
-deleteUser :: WU.LoginId -> AppM ()
-deleteUser u = do
-  b <- backend
-  liftIO (WU.deleteUser b u)
-
-authUser :: Text -> WU.PasswordPlain -> NominalDiffTime -> AppM (Maybe WU.SessionId)
-authUser email pw time = do
-  b <- backend
-  liftIO (WU.authUser b email pw time)
-
---authUser' = \e p t -> backend >>= \b -> liftIO $ WU.authUser b e p t
-
-deriveReader :: Name -> Name -> Q Exp
-deriveReader rd fn = do
-  info <- reify fn
-  arity <- maybe (reportError "Unable to get arity of name" >> return 0)
-        (return . functionLevels) 
-        (getType info)
-  varNames <- replicateM arity (newName "arg")
-  expr <- [| rd >>= \b -> liftIO $ fn b $(foldl1 AppE <$> mapM varE varNames) |]
-  if arity > 1
-     then [| const 2 |]
-     else [| const 1 |]
-
-
-
-{-
-in a very dumb way, what I want to do is:
-
-deriveFn WU.authUser
-1. inspect authUser's type
-2. drop the first function in the type
-splice in:
-deriveFn WU.authUser =
-  \e p t -> backend >>= \b -> liftIO (WU.authUser e p t)
-
-Notes --
-
-prints out the string representation of a function, specifically:
-
-> $(stringE . show =<< reify (mkName "W.authUser"))
-
-ClassOpI 
-  Web.Users.Types.authUser 
-  (ForallT 
-    [KindedTV b_1627464633 StarT] 
-    [AppT (ConT Web.Users.Types.UserStorageBackend) (VarT b_1627464633)] 
-    (AppT 
-      (AppT 
-        ArrowT 
-        (VarT b_1627464633))
-      (AppT 
-        (AppT 
-          ArrowT 
-          (ConT Data.Text.Internal.Text))
-        (AppT 
-          (AppT 
-            ArrowT 
-            (ConT Web.Users.Types.PasswordPlain))
-          (AppT 
-            (AppT 
-              ArrowT 
-              (ConT Data.Time.Clock.UTC.NominalDiffTime))
-            (AppT 
-              (ConT GHC.Types.IO)
-              (AppT 
-                (ConT GHC.Base.Maybe)
-                (ConT Web.Users.Types.SessionId)
-              )
-            )
-          )
-        )
-      )
-    )
-  )
-  Web.Users.Types.UserStorageBackend 
-  (Fixity 9 InfixL)
-
--}
---withAuthUser :: Text -> WU.PasswordPlain -> (QLUser -> Bool) -> (WU.LoginId -> IO r) -> AppM (Maybe r)
---withAuthUser email pw p f = do
---  b <- backend
---  liftIO (WU.withAuthUser b email pw p f)
--- src/Users/TH.hs
+++ src/Users/TH.hs
@@ -2,22 +2,53 @@
 
 module Users.TH where
 
+import Database.Persist.TH
+import Control.Monad
+import Control.Monad.Reader
+
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
 
-functionLevels :: Type -> Int
-functionLevels = go 0
-  where
-    go :: Int -> Type -> Int
-    go n (AppT (AppT ArrowT _) rest) =
-      go (n+1) rest
-    go n (ForallT _ _ rest) =
-      go n rest
-    go n _ =
-      n
+import Users.TH.Internal
 
-getType :: Info -> Maybe Type
-getType (ClassOpI _ t _ _) = Just t
-getType (DataConI _ t _ _) = Just t
-getType (VarI _ t _ _)     = Just t
-getType (TyVarI _ t)       = Just t
-getType _                  = Nothing
+import Web.Users.Persistent
+import Web.Users.Types
+
+deriveReader :: Name -> DecsQ
+deriveReader rd =
+  mapM (decForFunc rd) 
+    [ 'destroyUserBackend
+    , 'housekeepBackend
+    , 'getUserIdByName
+    , 'getUserById
+    , 'listUsers
+    , 'countUsers
+    , 'createUser
+    , 'updateUser
+    , 'updateUserDetails
+    , 'authUser
+    , 'deleteUser
+    ]
+
+decForFunc :: Name -> Name -> Q Dec
+decForFunc reader fn = do
+  info <- reify fn
+  arity <- maybe (reportError "Unable to get arity of name" >> return 0)
+        (return . functionLevels) 
+        (getType info)
+  varNames <- replicateM (arity - 1) (newName "arg")
+  b <- newName "b"
+  let fnName = mkName . nameBase $ fn
+      bound  = AppE (VarE '(>>=)) (VarE reader)
+      binder = AppE bound . LamE [VarP b]
+      varExprs = map VarE (b : varNames)
+      fullExpr   = foldl AppE (VarE fn) varExprs
+      liftedExpr = AppE (VarE (mkName "liftIO")) fullExpr
+  if arity > 1 
+     then do
+      let lambdaPat = LamE (map VarP varNames) (binder liftedExpr)
+      return $
+        FunD fnName [Clause [] (NormalB lambdaPat) []]
+     else do
+       let noLambda = binder liftedExpr
+       return $ FunD fnName [Clause [] (NormalB noLambda) []]
