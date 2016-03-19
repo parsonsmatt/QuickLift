11d7dc3cb2bb0c0c522376daef253e8cd63ca735
Matt Parsons <parsonsmatt@gmail.com>
2016-03-19 17:45:23 -0400

    More parsing, more specs

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Parser where
 
 import           Control.Lens
 import           Data.List             (genericReplicate)
+import           Data.Scientific
 import           Data.Text             (Text)
 import qualified Data.Text             as T
 import           Text.Megaparsec       as M
 import qualified Text.Megaparsec.Lexer as L
+import Data.Function
 
 data Lift
     = Lift
@@ -17,7 +20,7 @@ data Lift
 
 data Set
     = Set
-    { _weight :: Integer
+    { _weight :: Scientific
     , _reps   :: Integer
     } deriving (Eq, Show)
 
@@ -30,31 +33,50 @@ parse :: Parsec Text a -> String -> Text -> Either ParseError a
 parse = M.parse
 
 session :: Parsec Text Session
-session = Session <$> many liftParser
+session = Session <$> liftParser `sepBy` eol
 
 liftParser :: Parsec Text Lift
 liftParser = Lift <$> liftName <*> liftSets
 
 liftName :: Parsec Text Text
-liftName = T.pack <$> (space *> someTill anyChar (char ':') <* space)
+liftName = T.pack <$> (space' *> someTill anyChar (char ':') <* space)
 
 liftSets :: Parsec Text [Set]
-liftSets = concat <$> many setLine
+liftSets = concat <$> setLine `sepBy` eol
 
 setLine :: Parsec Text [Set]
 setLine = do
-    s <- Set <$> integer <*> xThenInt
-    repeats <- xThenInt <* skipComma
-    return $ genericReplicate repeats s
+    weight' <- decimal <?> "Attempting to get weight"
+    firstOff <- repsxsets
+    reps'repeats <- fix $ \go -> repsxsets >>= \case
+                                               (1,1) -> return []
+                                               this  -> (this :) <$> go
+    let sets' = firstOff : reps'repeats >>= uncurry (flip genericReplicate)
+    return . map (Set weight') $ sets'
+
+repsxsets :: Parsec Text (Integer, Integer)
+repsxsets = do
+    reps_ <- xThenInt <?> "Trying to get the reps..."
+    repeats <- xThenInt <* skipComma <?> "Trying to get the repeats."
+    return (reps_, repeats)
 
 xThenInt :: Parsec Text Integer
-xThenInt = option 1 $ try (skipChar 'x' >> integer)
+xThenInt = option 1 $ try (space' >> skipChar 'x' >> integer)
 
 lexeme :: Parsec Text a -> Parsec Text a
-lexeme = L.lexeme space
+lexeme = L.lexeme space'
 
 comma :: Parsec Text Char
 comma = lexeme (char ',')
+
+decimal :: Parsec Text Scientific
+decimal = either (`scientific` 0) fromFloatDigits <$> number
+
+float :: Parsec Text Double
+float = lexeme L.float
+
+number :: Parsec Text (Either Integer Double)
+number = lexeme L.number
 
 integer :: Parsec Text Integer
 integer = lexeme L.integer
@@ -64,3 +86,6 @@ skipChar = lexeme . skipMany . char'
 
 skipComma :: Parsec Text ()
 skipComma = skipChar ','
+
+space' :: Parsec Text ()
+space' = skipMany (oneOf " \t")
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -4,13 +4,11 @@
 
 module ParserSpec where
 
+import Data.Scientific
 import Test.Hspec
--- import Text.QuasiText
+import Text.QuasiText
 import Data.Either
 import Parser
-
-skip :: String -> t -> Expectation
-skip s _ = pendingWith s
 
 spec :: Spec
 spec = describe "Parser" $ do
@@ -23,20 +21,89 @@ spec = describe "Parser" $ do
             p "Squat" `shouldSatisfy` isLeft
     describe "setLine" $ do
         let p = parse setLine ""
-        it "parses a set line" $ do
+        it "parses a set line only weight" $ do
+            p "100" `shouldBe`
+                Right [Set 100 1]
+        it "parses a set with reps" $ do
             p "100 x 10" `shouldBe`
                 Right [Set 100 10]
         it "repeats a set" $ do
             p "100 x 10 x 2" `shouldBe`
                 Right (replicate 2 (Set 100 10))
-        it "no reps assumes to be 1 rep" $ do
-            p "100" `shouldBe`
-                Right [Set 100 1]
-        it "can work with commas" . skip "not implemented" $ do
+        it "can work with commas" $ do
             p "100 x 10, 5" `shouldBe`
                 Right [Set 100 10, Set 100 5]
-        it "can work with commas and sets" . skip "not implemented" $ do
+        it "can work with commas and sets" $ do
             p "100 x 10 x 2, 5" `shouldBe`
                 Right [Set 100 10, Set 100 10, Set 100 5]
             p "100 x 10 x 2, 5 x 2" `shouldBe`
                 Right [Set 100 10, Set 100 10, Set 100 5, Set 100 5]
+    describe "decimal" $ do
+        let p = parse decimal ""
+        it "parses ints" $ do
+            p "10" `shouldBe` Right (scientific 10 0)
+        it "parses floats" $ do
+            p "10.5" `shouldBe` Right (scientific 105 (-1))
+            p "10.25" `shouldBe` Right (scientific 1025 (-2))
+    describe "xThenInt" $ do
+        let p = parse xThenInt ""
+        it "Skips an optional x" $ do
+            p "10" `shouldBe` Right 10
+        it "succeeds with an x" $ do
+            p "x10" `shouldBe` Right 10
+        it "doesn't care about spaces" $ do
+            p " x 10" `shouldBe` Right 10
+        it "defaults to 1" $ do
+            p "" `shouldBe` Right 1
+    describe "repsxsets" $ do
+        let p = parse repsxsets ""
+        it "parses full format correctly" $ do
+            p "10 x 10" `shouldBe` Right (10, 10)
+        it "parses missing sets correctly" $ do
+            p "10" `shouldBe` Right (10, 1)
+        it "parses blank as (1, 1)" $ do
+            p "" `shouldBe` Right (1, 1)
+
+    describe "liftSets" $ do
+        let p = parse liftSets ""
+        it "parses many lift sets" $ do
+            let s = [embed|
+10
+10 x 2
+10 x 2 x 3
+|]
+            p s `shouldBe`
+                Right [Set 10 1, Set 10 2, Set 10 2, Set 10 2, Set 10 2]
+
+    describe "session" $ do
+        let p = parse session ""
+            ex =  Lift "Squat" [ Set 100 1, Set 110 2, Set 115 3,
+                               Set 115 3, Set 115 3, Set 100 8, Set 100 5, Set
+                               102.5 3, Set 102.5 3, Set 102.5 3, Set 102.5 5]
+        it "parses an empty lift" $ do
+            p "" `shouldBe` Right (Session [])
+        it "parses a single set" $ do
+            p "Squat: 100" `shouldBe` Right (Session [Lift "Squat" [Set 100 1]])
+        it "is good with a newline" $ do
+            p "Squat:\n100" `shouldBe` Right (Session [Lift "Squat" [Set 100 1]])
+        it "parses a single lift" $ do
+            let s = [embed|
+Squat:
+100
+110 x 2
+115 x 3 x 3
+100 x 8, 5
+102.5 x 3 x 3, 5
+
+|]
+            p s `shouldBe` Right (Session [ex])
+        it "parses two lifts" $ do
+            pendingWith "Fails, attempting to parse 'Squat' as another set of the first one."
+            let s = [embed|
+Squat:
+100
+
+Squat:
+100
+|]
+            p s `shouldBe` Right (Session (replicate 2 (Lift "Squat" [Set 100 1])))
